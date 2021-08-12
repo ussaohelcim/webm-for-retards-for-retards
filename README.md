f24fc9e39c03d885fa5f631e4c38de950f4c7f10
ussaohelcim <57050328+ussaohelcim@users.noreply.github.com>
2021-08-11 22:33:38 -0300

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # webm-for-retards-for-retards
-Simple as fuck ffmpeg wrapper for 4chan video upload.
+Simple as fuck ffmpeg wrapper for 4chan webm video upload.
+
+# How to use
+
+[Click here to download]()
+
+- open the program
+- select the video
+- wait
+- the program will close
+- the webm will be avaiable in the same directory as this program
+- the output will be a webm without audio and 1 minute and 29 seconds maximun lenght
---
+++ main.py
@@ -0,0 +1,25 @@
+import subprocess
+from tkinter import *
+from tkinter import filedialog
+import time
+
+def GetUnixTime():
+	tm = tm = str(time.time())
+	tm = tm[:len(tm)-4]
+	tm = tm.replace('.',"")
+	return tm
+
+root = Tk()
+
+root.title("webm for retards for retards")
+root.geometry("640x40")
+
+txt = Label(root, text="Just wait a little, maybe your video will be converted.").pack()
+
+pathVideo = filedialog.askopenfilename( 
+	initialdir=".",
+	title="select your video, bro", 
+	filetypes=(("videos files","*.mp4"),("other","*.avi"),("everything","*"))
+	)
+
+a = subprocess.run('ffmpeg -t 00:01:29 -i "'+pathVideo+'" -an '+GetUnixTime()+".webm -y ")
